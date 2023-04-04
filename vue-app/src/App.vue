147b6103d05670d366bba6ee1617f13447d9a9a4
lkmdev <lkmdev01@gmail.com>
2023-04-04 14:44:48 -0300

    primeiro commit

---
+++ vue-app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <HelloWorld />
+    {{ $store.state.user.first_name }}
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    
+  },
+  created(){
+    
+  }
+}
+</script>
---
+++ vue-app/src/store/index.js
@@ -0,0 +1,23 @@
+import { createStore } from 'vuex'
+
+
+export default createStore({
+  state:{
+    user: {
+      first_name: "Lukas",
+      last_name: "Martins",
+      email: "exemplo@gmail.com"
+    },
+    product: [],
+    cart: []
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  },
+  getters: {
+
+  }
+})
